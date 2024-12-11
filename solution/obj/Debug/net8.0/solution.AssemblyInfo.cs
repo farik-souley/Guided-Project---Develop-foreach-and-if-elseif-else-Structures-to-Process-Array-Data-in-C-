76bc0b90993526f1a0fb6ac5e0ed7592998f1093
farik <daouudasouleymanou@gmail.com>
2024-12-11 10:12:52 +0100

    Updating Solution

--- solution/obj/Debug/net8.0/solution.AssemblyInfo.cs
+++ solution/obj/Debug/net8.0/solution.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("solution")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+587834ce02c438041cd3b1950256887878e0a1a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5306ca1ca07dd6f5f67a36c13a1b672f467c617f")]
 [assembly: System.Reflection.AssemblyProductAttribute("solution")]
 [assembly: System.Reflection.AssemblyTitleAttribute("solution")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
